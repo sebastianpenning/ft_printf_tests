6cb488c01afe65fbaef5c14164b001b073352150
spenning <whimizera@mailbox.org>
2023-12-21 18:01:41 +0100

    modification to %c tests

--- ft_printf_c.c
+++ ft_printf_c.c
@@ -22,7 +22,7 @@ void category_c()
 	freopen("ft_temp.txt", "w", stdout); 
 	ft_outcome = ft_printf("lol %c", '1'); 
 	freopen("temp.txt", "w", stdout);
-	outcome = printf("lolc %c", '1');
+	outcome = printf("lol %c", '1');
 	freopen("/dev/tty", "w", stdout);
 	file_outcome = compare_files(test_count);
 	len_outcome = compare_length(test_count, ft_outcome, outcome);
@@ -32,6 +32,63 @@ void category_c()
 	else
 		printf("OK\n");
 	printf("-------------------\n");
+	test_count++;
+	freopen("ft_temp.txt", "w", stdout); 
+	ft_outcome = ft_printf("lol %c", '\200'); 
+	freopen("temp.txt", "w", stdout);
+	outcome = printf("lol %c", '\200');
+	freopen("/dev/tty", "w", stdout);
+	file_outcome = compare_files(test_count);
+	len_outcome = compare_length(test_count, ft_outcome, outcome);
+	printf("test %d: ", test_count);
+	if(len_outcome != 0 || file_outcome != 0) 
+		printf("FAIL\n");
+	else
+		printf("OK\n");
+	printf("-------------------\n");
+	test_count++;
+	freopen("ft_temp.txt", "w", stdout); 
+	ft_outcome = ft_printf("%c%c%c", '\0', 'l', -42); 
+	freopen("temp.txt", "w", stdout);
+	outcome = printf("%c%c%c", '\0', 'l', -42);
+	freopen("/dev/tty", "w", stdout);
+	file_outcome = compare_files(test_count);
+	len_outcome = compare_length(test_count, ft_outcome, outcome);
+	printf("test %d: ", test_count);
+	if(len_outcome != 0 || file_outcome != 0) 
+		printf("FAIL\n");
+	else
+		printf("OK\n");
+	printf("-------------------\n");
+	test_count++;
+	freopen("ft_temp.txt", "w", stdout); 
+	ft_outcome = ft_printf("%c%c%c", 23647238, 265, -42); 
+	freopen("temp.txt", "w", stdout);
+	outcome = printf("%c%c%c",  23647238, 265, -42);
+	freopen("/dev/tty", "w", stdout);
+	file_outcome = compare_files(test_count);
+	len_outcome = compare_length(test_count, ft_outcome, outcome);
+	printf("test %d: ", test_count);
+	if(len_outcome != 0 || file_outcome != 0) 
+		printf("FAIL\n");
+	else
+		printf("OK\n");
+	printf("-------------------\n");
+	test_count++;
+	freopen("ft_temp.txt", "w", stdout); 
+	ft_outcome = ft_printf("%c%c%c", 0, -1, 0); 
+	freopen("temp.txt", "w", stdout);
+	outcome = printf("%c%c%c",  0, -1, 0);
+	freopen("/dev/tty", "w", stdout);
+	file_outcome = compare_files(test_count);
+	len_outcome = compare_length(test_count, ft_outcome, outcome);
+	printf("test %d: ", test_count);
+	if(len_outcome != 0 || file_outcome != 0) 
+		printf("FAIL\n");
+	else
+		printf("OK\n");
+	printf("-------------------\n");
+
 
 	remove("ft_temp.txt");
 	remove("temp.txt");
